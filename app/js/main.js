33f57f3c634ecb151637a038da68b9680a956043
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2020-12-26 16:47:40 +0300

    Сверстана главная страница , осталось сделать фильтры товаров

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,32 @@
+window.addEventListener('DOMContentLoaded', function () {
+      let productsWeekImg = document.querySelectorAll('.productsWeek__item-img');
+
+      function productsImgMargin() {
+            for (let i = 0; i < productsWeekImg.length; i++) {
+                  let heightProductsWeekImg = productsWeekImg[i].height;
+
+                  if (heightProductsWeekImg < 175 && heightProductsWeekImg >= 170) {
+                        productsWeekImg[i].style.marginTop = '13px';
+                  } else if (heightProductsWeekImg < 170 && heightProductsWeekImg > 165) {
+                        productsWeekImg[i].style.marginTop = '20px';
+                  } else if (heightProductsWeekImg < 165 && heightProductsWeekImg > 160) {
+                        productsWeekImg[i].style.marginTop = '30px';
+                  } else if (heightProductsWeekImg < 160 && heightProductsWeekImg > 155) {
+                        productsWeekImg[i].style.marginTop = '27px';
+                  } else if (heightProductsWeekImg < 150 && heightProductsWeekImg > 145) {
+                        productsWeekImg[i].style.marginTop = '23px';
+                  } else if (heightProductsWeekImg < 140 && heightProductsWeekImg > 130) {
+                        productsWeekImg[i].style.marginTop = '48px';
+                  } 
+            }
+      }
+      productsImgMargin();
+});
 $(function () {
       $('.hero-slider').slick({
             arrows: false,
             dots: true,
             fade: true
       });
-});
-
-window.addEventListener('DOMContentLoaded', function () {
-      let productsWeekImg = document.querySelectorAll('.productsWeek__item-img');
-      for (let i = 0; i < productsWeekImg.length; i++) {
-            let heightProductsWeekImg = productsWeekImg[i].height;
-
-            if (heightProductsWeekImg < 175 && heightProductsWeekImg >= 170) {
-                  productsWeekImg[i].style.marginTop = '13px';
-            } else if (heightProductsWeekImg < 170 && heightProductsWeekImg > 165) {
-                  productsWeekImg[i].style.marginTop = '20px';
-            } else if (heightProductsWeekImg < 165 && heightProductsWeekImg > 160) {
-                  productsWeekImg[i].style.marginTop = '30px';
-            } else if (heightProductsWeekImg < 160 && heightProductsWeekImg > 155) {
-                  productsWeekImg[i].style.marginTop = '27px';
-            } else if (heightProductsWeekImg < 150 && heightProductsWeekImg > 145) {
-                  productsWeekImg[i].style.marginTop = '23px';
-            } else if (heightProductsWeekImg < 140 && heightProductsWeekImg > 130) {
-                  productsWeekImg[i].style.marginTop = '48px';
-            }
-      }
+      $('.video__btn').fancybox({});
 });
